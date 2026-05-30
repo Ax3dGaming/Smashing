4035e0be813f464f924e3b630cfa434c4c21913b
Ax3dgaming <ax3dgaming@gmail.com>
2026-05-30 16:57:19 +0200

    Cleaned up the base code

---
+++ src/main/java/com/axedgaming/smashing/Smashing.java
@@ -0,0 +1,13 @@
+package com.axedgaming.smashing;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Smashing.MODID)
+public class Smashing {
+    public static final String MODID = "smashing";
+
+    public Smashing(IEventBus modEventBus) {
+
+    }
+}
